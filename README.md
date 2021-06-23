e4d7c55a97249e2d92243612c1d9c0550dfaca7a
tharvey <tharveyster@gmail.com>
2021-06-23 12:51:57 -0400

    Added comments, readme file, and screenshot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## Description
+This project is a work day event scheduler. It was created using `HTML`, `CSS`, `JavaScript`, `jQuery`, and `Moment.js`.
+
+The application determines the current day of the week and date, and displays them under the title.
+
+It also uses the current time to determine the background colors for each hour. Hours that have passed are displayed with a gray background, the current hour has a pink background, and future hours have a green background.
+
+When you select a row, type in an event, and press the save button, the event is saved to local storage, and will remain visible even after a refresh.
+
+## Page Link
+The project can be viewed at <https://tharveyster.github.io/work-day-scheduler/>.
+
+## Screen Shot
+![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/images/work-day-scheduler.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// Get current day of week and date
 var todaysDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(todaysDate);
 
+// Get current time
 var currentHour = moment().hour();
 
+// Run these functions to get events from local storage
 event09();
 event10();
 event11();
@@ -13,6 +16,7 @@ event15();
 event16();
 event17();
 
+// Assign each hour block to a variable
 var hour09 = parseInt(document.querySelector("#hour09").getAttribute("data-hour"));
 var hour10 = parseInt(document.querySelector("#hour10").getAttribute("data-hour"));
 var hour11 = parseInt(document.querySelector("#hour11").getAttribute("data-hour"));
@@ -23,6 +27,7 @@ var hour15 = parseInt(document.querySelector("#hour15").getAttribute("data-hour"
 var hour16 = parseInt(document.querySelector("#hour16").getAttribute("data-hour"));
 var hour17 = parseInt(document.querySelector("#hour17").getAttribute("data-hour"));
 
+// Determine background color for 9AM
 if (hour09 < currentHour) {
     $("#desc09").removeClass("present");
     $("#desc09").removeClass("future");
@@ -37,6 +42,7 @@ if (hour09 < currentHour) {
     $("#desc09").addClass("future");
 }
 
+// Determine background color for 10AM
 if (hour10 < currentHour) {
     $("#desc10").removeClass("present");
     $("#desc10").removeClass("future");
@@ -46,9 +52,12 @@ if (hour10 < currentHour) {
     $("#desc10").removeClass("future");
     $("#desc10").addClass("present");
 } else {
+    $("#desc10").removeClass("past");
+    $("#desc10").removeClass("present");
     $("#desc10").addClass("future");
 }
 
+// Determine background color for 11AM
 if (hour11 < currentHour) {
     $("#desc11").removeClass("present");
     $("#desc11").removeClass("future");
@@ -58,9 +67,12 @@ if (hour11 < currentHour) {
     $("#desc11").removeClass("future");
     $("#desc11").addClass("present");
 } else {
+    $("#desc11").removeClass("past");
+    $("#desc11").removeClass("present");
     $("#desc11").addClass("future");
 }
 
+// Determine background color for 12PM
 if (hour12 < currentHour) {
     $("#desc12").removeClass("present");
     $("#desc12").removeClass("future");
@@ -71,8 +83,11 @@ if (hour12 < currentHour) {
     $("#desc12").addClass("present");
 } else {
     $("#desc12").addClass("future");
+    $("#desc12").removeClass("past");
+    $("#desc12").removeClass("present");
 }
 
+// Determine background color for 1PM
 if (hour13 < currentHour) {
     $("#desc13").removeClass("present");
     $("#desc13").removeClass("future");
@@ -82,9 +97,12 @@ if (hour13 < currentHour) {
     $("#desc13").removeClass("future");
     $("#desc13").addClass("present");
 } else {
+    $("#desc13").removeClass("past");
+    $("#desc13").removeClass("present");
     $("#desc13").addClass("future");
 }
 
+// Determine background color for 2PM
 if (hour14 < currentHour) {
     $("#desc14").removeClass("present");
     $("#desc14").removeClass("future");
@@ -94,9 +112,12 @@ if (hour14 < currentHour) {
     $("#desc14").removeClass("future");
     $("#desc14").addClass("present");
 } else {
+    $("#desc14").removeClass("past");
+    $("#desc14").removeClass("present");
     $("#desc14").addClass("future");
 }
 
+// Determine background color for 3PM
 if (hour15 < currentHour) {
     $("#desc15").removeClass("present");
     $("#desc15").removeClass("future");
@@ -106,9 +127,12 @@ if (hour15 < currentHour) {
     $("#desc15").removeClass("future");
     $("#desc15").addClass("present");
 } else {
+    $("#desc15").removeClass("past");
+    $("#desc15").removeClass("present");
     $("#desc15").addClass("future");
 }
 
+// Determine background color for 4PM
 if (hour16 < currentHour) {
     $("#desc16").removeClass("present");
     $("#desc16").removeClass("future");
@@ -118,9 +142,12 @@ if (hour16 < currentHour) {
     $("#desc16").removeClass("future");
     $("#desc16").addClass("present");
 } else {
+    $("#desc16").removeClass("past");
+    $("#desc16").removeClass("present");
     $("#desc16").addClass("future");
 }
 
+// Determine background color for 5PM
 if (hour17 < currentHour) {
     $("#desc17").removeClass("present");
     $("#desc17").removeClass("future");
@@ -130,102 +157,113 @@ if (hour17 < currentHour) {
     $("#desc17").removeClass("future");
     $("#desc17").addClass("present");
 } else {
+    $("#desc17").removeClass("past");
+    $("#desc17").removeClass("present");
     $("#desc17").addClass("future");
 }
 
+// Event listener and local storage input/output for 9AM
 function event09() {
     var outputArea09 = document.querySelector("#desc09");
     var saveBtn = document.querySelector("#desc09Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event09", outputArea09.value);
     });
 
     outputArea09.value = localStorage.getItem("event09");
 }
 
+// Event listener and local storage input/output for 10AM
 function event10() {
     var outputArea10 = document.querySelector("#desc10");
     var saveBtn = document.querySelector("#desc10Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event10", outputArea10.value);
     });
 
     outputArea10.value = localStorage.getItem("event10");
 }
 
+// Event listener and local storage input/output for 11AM
 function event11() {
     var outputArea11 = document.querySelector("#desc11");
     var saveBtn = document.querySelector("#desc11Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event11", outputArea11.value);
     });
 
     outputArea11.value = localStorage.getItem("event11");
 }
 
+// Event listener and local storage input/output for 12PM
 function event12() {
     var outputArea12 = document.querySelector("#desc12");
     var saveBtn = document.querySelector("#desc12Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event12", outputArea12.value);
     });
 
     outputArea12.value = localStorage.getItem("event12");
 }
 
+// Event listener and local storage input/output for 1PM
 function event13() {
     var outputArea13 = document.querySelector("#desc13");
     var saveBtn = document.querySelector("#desc13Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event13", outputArea13.value);
     });
 
     outputArea13.value = localStorage.getItem("event13");
 }
 
+// Event listener and local storage input/output for 2PM
 function event14() {
     var outputArea14 = document.querySelector("#desc14");
     var saveBtn = document.querySelector("#desc14Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event14", outputArea14.value);
     });
 
     outputArea14.value = localStorage.getItem("event14");
 }
 
+// Event listener and local storage input/output for 3PM
 function event15() {
     var outputArea15 = document.querySelector("#desc15");
     var saveBtn = document.querySelector("#desc15Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event15", outputArea15.value);
     });
 
     outputArea15.value = localStorage.getItem("event15");
 }
 
+// Event listener and local storage input/output for 4PM
 function event16() {
     var outputArea16 = document.querySelector("#desc16");
     var saveBtn = document.querySelector("#desc16Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event16", outputArea16.value);
     });
 
     outputArea16.value = localStorage.getItem("event16");
 }
 
+// Event listener and local storage input/output for 5PM
 function event17() {
     var outputArea17 = document.querySelector("#desc17");
     var saveBtn = document.querySelector("#desc17Save");
 
-    saveBtn.addEventListener("click", function() {
+    saveBtn.addEventListener("click", function () {
         localStorage.setItem("event17", outputArea17.value);
     });
 
