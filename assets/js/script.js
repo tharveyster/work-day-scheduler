0fcb9eea5d0f8be6071132280d202c97177454d3
tharvey <tharveyster@gmail.com>
2021-07-06 14:06:26 -0400

    Shortened code by adding a jQuery for each loop to determine background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var blockHour;
+
 // Get current day of week and date
 var todaysDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(todaysDate);
@@ -16,151 +18,20 @@ event15();
 event16();
 event17();
 
-// Assign each hour block to a variable
-var hour09 = parseInt(document.querySelector("#hour09").getAttribute("data-hour"));
-var hour10 = parseInt(document.querySelector("#hour10").getAttribute("data-hour"));
-var hour11 = parseInt(document.querySelector("#hour11").getAttribute("data-hour"));
-var hour12 = parseInt(document.querySelector("#hour12").getAttribute("data-hour"));
-var hour13 = parseInt(document.querySelector("#hour13").getAttribute("data-hour"));
-var hour14 = parseInt(document.querySelector("#hour14").getAttribute("data-hour"));
-var hour15 = parseInt(document.querySelector("#hour15").getAttribute("data-hour"));
-var hour16 = parseInt(document.querySelector("#hour16").getAttribute("data-hour"));
-var hour17 = parseInt(document.querySelector("#hour17").getAttribute("data-hour"));
-
-// Determine background color for 9AM
-if (hour09 < currentHour) {
-    $("#desc09").removeClass("present");
-    $("#desc09").removeClass("future");
-    $("#desc09").addClass("past");
-} else if (hour09 === currentHour) {
-    $("#desc09").removeClass("past");
-    $("#desc09").removeClass("future");
-    $("#desc09").addClass("present");
-} else {
-    $("#desc09").removeClass("past");
-    $("#desc09").removeClass("present");
-    $("#desc09").addClass("future");
-}
-
-// Determine background color for 10AM
-if (hour10 < currentHour) {
-    $("#desc10").removeClass("present");
-    $("#desc10").removeClass("future");
-    $("#desc10").addClass("past");
-} else if (hour10 === currentHour) {
-    $("#desc10").removeClass("past");
-    $("#desc10").removeClass("future");
-    $("#desc10").addClass("present");
-} else {
-    $("#desc10").removeClass("past");
-    $("#desc10").removeClass("present");
-    $("#desc10").addClass("future");
-}
-
-// Determine background color for 11AM
-if (hour11 < currentHour) {
-    $("#desc11").removeClass("present");
-    $("#desc11").removeClass("future");
-    $("#desc11").addClass("past");
-} else if (hour11 === currentHour) {
-    $("#desc11").removeClass("past");
-    $("#desc11").removeClass("future");
-    $("#desc11").addClass("present");
-} else {
-    $("#desc11").removeClass("past");
-    $("#desc11").removeClass("present");
-    $("#desc11").addClass("future");
-}
-
-// Determine background color for 12PM
-if (hour12 < currentHour) {
-    $("#desc12").removeClass("present");
-    $("#desc12").removeClass("future");
-    $("#desc12").addClass("past");
-} else if (hour12 === currentHour) {
-    $("#desc12").removeClass("past");
-    $("#desc12").removeClass("future");
-    $("#desc12").addClass("present");
-} else {
-    $("#desc12").addClass("future");
-    $("#desc12").removeClass("past");
-    $("#desc12").removeClass("present");
-}
-
-// Determine background color for 1PM
-if (hour13 < currentHour) {
-    $("#desc13").removeClass("present");
-    $("#desc13").removeClass("future");
-    $("#desc13").addClass("past");
-} else if (hour13 === currentHour) {
-    $("#desc13").removeClass("past");
-    $("#desc13").removeClass("future");
-    $("#desc13").addClass("present");
-} else {
-    $("#desc13").removeClass("past");
-    $("#desc13").removeClass("present");
-    $("#desc13").addClass("future");
-}
-
-// Determine background color for 2PM
-if (hour14 < currentHour) {
-    $("#desc14").removeClass("present");
-    $("#desc14").removeClass("future");
-    $("#desc14").addClass("past");
-} else if (hour14 === currentHour) {
-    $("#desc14").removeClass("past");
-    $("#desc14").removeClass("future");
-    $("#desc14").addClass("present");
-} else {
-    $("#desc14").removeClass("past");
-    $("#desc14").removeClass("present");
-    $("#desc14").addClass("future");
-}
-
-// Determine background color for 3PM
-if (hour15 < currentHour) {
-    $("#desc15").removeClass("present");
-    $("#desc15").removeClass("future");
-    $("#desc15").addClass("past");
-} else if (hour15 === currentHour) {
-    $("#desc15").removeClass("past");
-    $("#desc15").removeClass("future");
-    $("#desc15").addClass("present");
-} else {
-    $("#desc15").removeClass("past");
-    $("#desc15").removeClass("present");
-    $("#desc15").addClass("future");
-}
-
-// Determine background color for 4PM
-if (hour16 < currentHour) {
-    $("#desc16").removeClass("present");
-    $("#desc16").removeClass("future");
-    $("#desc16").addClass("past");
-} else if (hour16 === currentHour) {
-    $("#desc16").removeClass("past");
-    $("#desc16").removeClass("future");
-    $("#desc16").addClass("present");
-} else {
-    $("#desc16").removeClass("past");
-    $("#desc16").removeClass("present");
-    $("#desc16").addClass("future");
-}
-
-// Determine background color for 5PM
-if (hour17 < currentHour) {
-    $("#desc17").removeClass("present");
-    $("#desc17").removeClass("future");
-    $("#desc17").addClass("past");
-} else if (hour17 === currentHour) {
-    $("#desc17").removeClass("past");
-    $("#desc17").removeClass("future");
-    $("#desc17").addClass("present");
-} else {
-    $("#desc17").removeClass("past");
-    $("#desc17").removeClass("present");
-    $("#desc17").addClass("future");
-}
+// Determine the background color for each hour
+$('.hour').each(function () {
+    blockHour = parseInt($(this).attr('data-hour'))
+    if (blockHour < currentHour) {
+        $(this).siblings('textarea').addClass('past');
+    } else if (blockHour == currentHour) {
+        $(this).siblings('textarea').removeClass('past');
+        $(this).siblings('textarea').addClass('present');
+    } else {
+        $(this).siblings('textarea').removeClass('past');
+        $(this).siblings('textarea').removeClass('present');
+        $(this).siblings('textarea').addClass('future');
+    }
+});
 
 // Event listener and local storage input/output for 9AM
 function event09() {
